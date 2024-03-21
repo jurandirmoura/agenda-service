e64926914a93eae00e524986ab402862eec7ee69
jurandir moura <jurandirmoura40@gmail.com>
2024-03-21 20:07:49 -0300

    commit deletando modulo spring security da api

--- src/main/java/br/com/mutztech/agenda/domain/repository/AgendaRepository.java
+++ src/main/java/br/com/mutztech/agenda/domain/repository/AgendaRepository.java
@@ -2,11 +2,13 @@ package br.com.mutztech.agenda.domain.repository;
 
 import br.com.mutztech.agenda.domain.entity.Agenda;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface AgendaRepository extends JpaRepository<Agenda, UUID> {
     Optional<Agenda> findByHorario(LocalDateTime horario);
 }
